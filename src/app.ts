543e27ffb676f18bd4123197befd6abf5ecc1900
do0ori <fuzzydo0ori@gmail.com>
2024-03-26 02:54:31 +0900

    refactor: Update app.ts to include additional middleware and routers

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,29 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import "express-async-errors";
+import cookieParser from 'cookie-parser';
+import cors from "cors";
+import { CORS_ALLOWED_ORIGIN } from "./settings";
+import usersRouter from "./routes/users.route";
+import notesRouter from "./routes/notes.route";
+import healthcheckRouter from "./routes/healthcheck.route";
+import errorHandler from "./middlewares/errorHandler.middleware";
 
 const app = express();
 
+app.use(
+    cors({
+        origin: CORS_ALLOWED_ORIGIN,
+        credentials: true
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
-    console.error(err);
-    res.sendStatus(500);
-});
+app.use("/api/", usersRouter);
+app.use("/api/notes", notesRouter);
+app.use("/api/healthcheck", healthcheckRouter);
+
+app.use(errorHandler);
 
 export { app };
