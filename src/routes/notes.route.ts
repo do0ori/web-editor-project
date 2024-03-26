2e5deac8d5aa628abbf960467b58a1a702a6db4a
do0ori <fuzzydo0ori@gmail.com>
2024-03-26 20:31:15 +0900

    feat(route): Add validation middleware to routes
    - Added validation middleware to the users and notes routes for validating user input.
    - Routes now use the validation middleware to ensure the correctness of request data before processing.

--- src/routes/notes.route.ts
+++ src/routes/notes.route.ts
@@ -2,13 +2,14 @@ import express from "express";
 import notesController from "../controllers/notes.controller";
 import { authenticateUser } from "../middlewares/authentication.middleware";
 import { authorizeNote } from "../middlewares/authorization.middleware";
+import validate from "../middlewares/validation.middleware";
 
 const router = express.Router();
 
 router.get("/", authenticateUser, notesController.getNotes);
-router.post("/", authenticateUser, notesController.createNote);
-router.get("/:id", authenticateUser, authorizeNote, notesController.getNote);
-router.put("/:id", authenticateUser, authorizeNote, notesController.updateNote);
-router.delete("/:id", authenticateUser, authorizeNote, notesController.deleteNote);
+router.post("/", authenticateUser, validate.note, notesController.createNote);
+router.get("/:id", authenticateUser, validate.noteId, authorizeNote, notesController.getNote);
+router.put("/:id", authenticateUser, validate.noteIdAndNote, authorizeNote, notesController.updateNote);
+router.delete("/:id", authenticateUser, validate.noteId, authorizeNote, notesController.deleteNote);
 
 export default router;
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import usersController from "../controllers/users.controller";
 import { authenticateUser } from "../middlewares/authentication.middleware";
+import validate from "../middlewares/validation.middleware";
 
 const router = express.Router();
 
-router.post("/login", usersController.logIn);
+router.post("/login", validate.user, usersController.logIn);
 router.post("/logout", usersController.logOut);
 router.get("/users/me", authenticateUser, usersController.userInfo);
-router.post("/users", usersController.signUp);
+router.post("/users", validate.user, usersController.signUp);
 
 export default router;
