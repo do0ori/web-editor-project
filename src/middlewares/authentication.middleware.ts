51a81f37c939ffa4678c67ae68efcb9925ef49a7
do0ori <fuzzydo0ori@gmail.com>
2024-03-28 17:17:33 +0900

    refactor(middleware): Specify JWT payload type and simplify error handling in authentication middleware

--- src/middlewares/authentication.middleware.ts
+++ src/middlewares/authentication.middleware.ts
@@ -1,23 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { IUser } from "../models/user.model";
 import { AuthenticationError } from "../errors/userFacing.error";
+import { JWTUserPayload } from "../services/users.service";
 
 declare module "express-serve-static-core" {
     interface Request {
-        user?: Omit<IUser, "encrypted_password">;
+        user?: JWTUserPayload;
     }
 }
 
 export const authenticateUser = async (req: Request, res: Response, next: NextFunction) => {
     const accessToken = req.cookies["access-token"];
 
-    if (!accessToken) {
-        throw new AuthenticationError();
-    }
+    if (!accessToken) throw new AuthenticationError();
 
     const user = jwt.verify(accessToken, process.env.JWT_SECRET!, { ignoreExpiration: false });
 
-    req.user = user as Omit<IUser, "encrypted_password">;
+    req.user = user as JWTUserPayload;
     next();
 };
