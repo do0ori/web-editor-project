36f01850516242aee25c5f28f1dd22fe6cef4ee8
do0ori <fuzzydo0ori@gmail.com>
2024-03-28 17:07:40 +0900

    refactor(controller): Separate business logic from controllers to services
    Moved the business logic related to notes and users from controllers to corresponding service files (`notes.service.ts` and `users.service.ts`) for better separation of concerns and maintainability.

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,26 +1,18 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
-import User from "../models/user.model";
 import { StatusCodes } from "http-status-codes";
-import { LoginFail } from "../errors/userFacing.error";
+import UsersService from "../services/users.service";
 
 const logIn = async (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body;
 
-    const user = await User.get(email, password);
+    const accessToken = await UsersService.logIn(email, password);
 
-    if (user) {
-        const accessToken = jwt.sign(user, process.env.JWT_SECRET!, { expiresIn: process.env.JWT_LIFETIME })
-
-        res.cookie("access-token", accessToken, {
-            sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
-            secure: process.env.NODE_ENV !== "development",
-            maxAge: 1000 * 60 * 60 * 24 * parseInt(process.env.JWT_LIFETIME!)
-        });
-        res.sendStatus(StatusCodes.OK);
-    } else {
-        throw new LoginFail();
-    }
+    res.cookie("access-token", accessToken, {
+        sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+        secure: process.env.NODE_ENV !== "development",
+        maxAge: 1000 * 60 * 60 * 24 * parseInt(process.env.JWT_LIFETIME!)
+    });
+    res.sendStatus(StatusCodes.OK);
 };
 
 const logOut = async (req: Request, res: Response, next: NextFunction) => {
@@ -35,16 +27,16 @@ const userInfo = async (req: Request, res: Response, next: NextFunction) => {
 const signUp = async (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body;
 
-    await User.create(email, password);
+    await UsersService.signUp(email, password);
 
     res.sendStatus(StatusCodes.CREATED);
 };
 
-const usersController = {
+const UsersController = {
     logIn,
     logOut,
     userInfo,
     signUp
 };
 
-export default usersController;
+export default UsersController;
