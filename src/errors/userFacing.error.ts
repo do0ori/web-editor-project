9ee6b8237668848fcb45a01ee7fc7888845e6b80
do0ori <fuzzydo0ori@gmail.com>
2024-03-28 17:15:06 +0900

    refactor(error): Renamed error classes and added new error classes for consistency and clarity

--- src/errors/userFacing.error.ts
+++ src/errors/userFacing.error.ts
@@ -1,9 +1,15 @@
 import { UserFacingError } from "./base.error";
 import { StatusCodes } from "http-status-codes";
 
-export class LoginFail extends UserFacingError {
+export class LogInFail extends UserFacingError {
     constructor() {
         super("로그인 정보가 올바르지 않습니다.", StatusCodes.UNAUTHORIZED);
+    }
+}
+
+export class SignUpFail extends UserFacingError {
+    constructor() {
+        super("회원가입 정보가 올바르지 않습니다.", StatusCodes.BAD_REQUEST);
     }
 }
 
@@ -24,6 +30,16 @@ export class NoteForbiddenError extends UserFacingError {
         super("해당 노트에 대한 권한이 없습니다.", StatusCodes.FORBIDDEN);
     }
 }
+
+export class CreateNoteFail extends UserFacingError {
+    constructor() {
+        super("노트 정보가 올바르지 않습니다.", StatusCodes.FORBIDDEN);
+    }
+}
+
+export class UpdateNoteFail extends CreateNoteFail { }
+
+export class DeleteNoteFail extends CreateNoteFail { }
 
 export class ValidationError extends UserFacingError {
     constructor() {
