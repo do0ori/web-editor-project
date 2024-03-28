fa1e5db6983c79824275f7ee212227f3bea56e20
do0ori <fuzzydo0ori@gmail.com>
2024-03-28 17:02:31 +0900

    feat(service): Add service layer for business logic separation
    Moved the business logic related code from the controllers and models into separate service files.
    This commit introduces `notes.service.ts` and `users.service.ts` to encapsulate the business logic for notes and users, respectively.

---
+++ src/services/notes.service.ts
@@ -0,0 +1,45 @@
+import { NoteNotFoundError, CreateNoteFail, UpdateNoteFail, DeleteNoteFail } from "../errors/userFacing.error";
+import Note from "../models/note.model";
+import NotesRepository from "../repositories/notes.repository";
+
+const getNotes = async (userId: number): Promise<Note[]> => {
+    const notes = await NotesRepository.getAll(userId);
+
+    return notes;
+};
+
+const getNote = async (noteId: number): Promise<Note> => {
+    const note = await NotesRepository.get(noteId);
+
+    if (note) return note;
+
+    throw new NoteNotFoundError();
+};
+
+const createNote = async (title: string, content: string, userId: number) => {
+    const result = await NotesRepository.create(title, content, userId);
+
+    if (result.affectedRows !== 1) throw new CreateNoteFail();
+};
+
+const updateNote = async (noteId: number, title: string, content: string) => {
+    const result = await NotesRepository.update(noteId, title, content);
+
+    if (result.affectedRows !== 1) throw new UpdateNoteFail();
+};
+
+const deleteNote = async (noteId: number) => {
+    const result = await NotesRepository.remove(noteId);
+
+    if (result.affectedRows !== 1) throw new DeleteNoteFail();
+};
+
+const NotesService = {
+    getNotes,
+    getNote,
+    createNote,
+    updateNote,
+    deleteNote
+};
+
+export default NotesService;
